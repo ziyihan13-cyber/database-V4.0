794e6375683d8602de18bf00113f44d1d1907f8b
ziyihan13-cyber <ziyihan13@gmail.com>
2026-04-10 14:37:57 +0800

    Add files via upload

---
+++ do_copy.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Copying files...
+copy "d:\Desktop\Tare\hanziyi_workstation\沙盘查看.html" "d:\Desktop\Tare\hanziyi_workstation\sandbox-dashboard-release\沙盘查看.html"
+copy "d:\Desktop\Tare\hanziyi_workstation\沙盘仪表盘-带分享.html" "d:\Desktop\Tare\hanziyi_workstation\sandbox-dashboard-release\沙盘仪表盘-带分享.html"
+echo Done!
+pause
